12392fbea482429ba384c9cd9862b8ab0b62c98e
bingo8670 <307286130@qq.com>
2017-06-28 09:58:35 +0800

    show group's creator info but seed don't work

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Initialize Admin User
+if User.find_by(email: "admin@test.com").nil?
+  u = User.new
+  u.email = "admin@test.com"           # 可以改成自己的 email
+  u.password = "123456"                # 最少要六码
+  u.password_confirmation = "123456"   # 最少要六码
+  u.is_admin = true
+  u.save
+  puts "Admin 已经建立好了，帐号为#{u.email}, 密码为#{u.password}"
+else
+  puts "Admin 已经建立过了，脚本跳过该步骤。"
+end
+
+# Initialize Group
+
+Group.create!(title: "罗振宇",
+  description: "罗辑思维",
+  creator: "admin@test.com"
+  )
+Group.create!(title: "李笑来",
+  description: "通往财富自由之路",
+  creator: "admin@test.com"
+  )
+Group.create!(title: "吴军",
+  description: "硅谷来信",
+  creator: "admin@test.com"
+  )
+Group.create!(title: "吴伯凡",
+  description: "伯凡日知录",
+  creator: "admin@test.com"
+  )
